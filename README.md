498cbd115f8d10da29a09e8fe9de3564cc7dd8f5
Jacob Wert <jacob.wert1@gmail.com>
2024-12-14 10:50:03 -0800

    Remove target directory from git tracking

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Advent_Of_Code_2024
+
+Code repository for the 2024 [Advent of Code](https://adventofcode.com/)
--- day_01/target/debug/deps/day_01-ac34eff97a2fbbe2.d
+++
@@ -1,6 +0,0 @@
-/Users/jacobwert/Documents/Projects/Advent_Of_Code_2024/day_01/target/debug/deps/day_01-ac34eff97a2fbbe2: src/main.rs src/utils.rs
-
-/Users/jacobwert/Documents/Projects/Advent_Of_Code_2024/day_01/target/debug/deps/day_01-ac34eff97a2fbbe2.d: src/main.rs src/utils.rs
-
-src/main.rs:
-src/utils.rs:
